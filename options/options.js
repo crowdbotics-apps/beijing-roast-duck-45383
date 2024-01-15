45f96716f5ae3e4cf65d0313676548ee668aca77
Crowdbotics <team@crowdbotics.com>
2024-01-15 08:56:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "beijing-roast-duck-45383",
+  url: "https://beijing-roast-duck-45383.botics.co",
+  api: "https://beijing-roast-duck-45383.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
